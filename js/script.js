28fbf4d73b4eeac7e2a3f99b1961cdb13323b36b
yvelious <yvelious@gmail.com>
2013-06-19 08:18:44 +0200

    add responsive

--- js/script.js
+++ js/script.js
@@ -52,14 +52,47 @@ $(function(){
 /* scrollable
  /* ---------------------------------------------------------------------- */
 
-$(function() {
+$(function ts() {
 
-    $(".b-page-wrap").scrollable({ circular: true, mousewheel: false,  vertical:true }).navigator({
-        navi: ".b-nav",
-        naviItem: 'li',
-        activeClass: 'active',
-        history: true
+    var _page =  $(window).width();
+
+    if (_page > 768) {
+
+            $(".b-page-wrap").scrollable({ circular: true, mousewheel: false,  vertical:true }).navigator({
+            navi: ".b-nav",
+            naviItem: 'li',
+            activeClass: 'active',
+            history: true
+
+            });
+    } else {
+
+
+         // Add smooth-scroll class to your links
+    $('.b-nav li a').on('click', function(e) {
+        e.preventDefault();
+
+        // Get href of link
+        var scrollTarget = $(this).attr('href');
+
+        // Get target position from top of the page
+        var targetPosition = $(scrollTarget).offset().top;
+
+        $('html,body').animate({scrollTop: targetPosition}, 300);
 
     });
+
+    }
+
 });
 
+
+
+
+/* bnt download CV top 
+ /* ---------------------------------------------------------------------- */
+
+$(".navbar-toggle").click(function () {
+$(".b-circle__links_cv_top").toggle();
+
+});
