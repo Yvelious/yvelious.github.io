75b4ac5de0bc47941eb2e5aa8504bb1db3cafeae
Stanislav Levyshchev <yvelious@gmail.com>
2025-06-04 21:52:43 +0200

    Remove jQuery and old library references from project

--- src/js/script.js
+++ src/js/script.js
@@ -28,72 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
 /* add to accordion
  /* ---------------------------------------------------------------------- */
 
-$(function () {
-    $(".b-accordion .accordion-group").click(function () {
-            $(".accordion-group").removeClass("active");
-            $(this).addClass("active")
-        }
-    );
-});
-
-/* tabs
- /* ---------------------------------------------------------------------- */
-
-
-$(function () {
-    $('#myTab a:first').tab('show');
-})
-
-
-/* menu
- /* ---------------------------------------------------------------------- */
-
-
-/* darkbox
- /* ---------------------------------------------------------------------- */
-
-$(function () {
-    $('a[data-darkbox=darkbox]').darkbox();
-})
-
-
-/* scrollable
- /* ---------------------------------------------------------------------- */
-
-$(function ts() {
-
-    var _page = $(window).width();
-
-    if (_page > 768) {
-        /*  $(".b-page-wrap").scrollable({ circular: false, mousewheel: false,  vertical:true }).navigator({
-          navi: ".b-nav",
-          naviItem: 'li',
-          activeClass: '',
-          history: true
-          });*/
-    } else {
-
-
-        // Add smooth-scroll class to your links
-        $('.b-header li a').on('click', function (e) {
-            /* e.preventDefault();
-
-             // Get href of link
-             var scrollTarget = $(this).attr('href');
-
-             // Get target position from top of the page
-             var targetPosition = $(scrollTarget).offset().top;
-
-             $('html,body').animate({scrollTop: targetPosition}, 300);
-     */
-        });
-
-    }
-
-});
-
-
-/* bnt download CV top
- /* ---------------------------------------------------------------------- */
-
+// $(function () {
+//     $(".b-accordion .accordion-group").click(function () {
+//             $(".accordion-group").removeClass("active");
+//             $(this).addClass("active")
+//         }
+//     );
+// });
 
