05b5cd8bc93ceeded6239fb3ca530d78547a00af
Stanislav Levyshchev <yvelious@gmail.com>
2025-08-09 14:04:06 +0200

    Minor update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,99 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const isProd = process.env.NODE_ENV === "production";
 const isDevServer = process.env.NODE_ENV === "server";
 
 const getCssFilename = (isProd, chunkName) => {
     if (isProd) {
         // needs because a stringed case created main instead of enhance
-        if (chunkName === 'main') {
-            return 'enhanced.[contenthash].css';
+        if (chunkName === "main" || chunkName === "enhanced") {
+            return "enhanced.[contenthash].css";
         }
+
     }
-    return '[name].css';
+    return "[name].css";
 };
 
 class optimizedCriticalandEnhancedCSSPlugin {
     apply(compiler) {
-        compiler.hooks.compilation.tap('InlineCriticalCSSPlugin', (compilation) => {
-            HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tap(
-                'InlineCriticalCSSPlugin',
-                (data) => {
-                    // get a source of critical CSS
-                    const criticalCSS = compilation.assets['critical.css']
-                        ? compilation.assets['critical.css'].source()
-                        : '';
+        compiler.hooks.compilation.tap(
+            "InlineCriticalCSSPlugin",
+            (compilation) => {
+                HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tap(
+                    "InlineCriticalCSSPlugin",
+                    (data) => {
+                        // get a source of critical CSS
+                        const criticalCSS = compilation.assets["critical.css"]
+                            ? compilation.assets["critical.css"].source()
+                            : "";
 
-                    // create tag <style> with critical CSS
-                    const inlineStyleTag = {
-                        tagName: 'style',
-                        voidTag: false,
-                        attributes: {
-                            type: 'text/css',
-                            id: 'critical-css',
-                        },
-                        innerHTML: criticalCSS,
-                    };
+                        // create tag <style> with critical CSS
+                        const inlineStyleTag = {
+                            tagName: "style",
+                            voidTag: false,
+                            attributes: {
+                                type: "text/css",
+                                id: "critical-css",
+                            },
+                            innerHTML: criticalCSS,
+                        };
 
-                    // remove a link to critical CSS
-                    data.assetTags.styles = data.assetTags.styles.filter(tag =>
-                        !tag.attributes?.href?.includes('critical')
-                    );
-                    data.assetTags.styles = data.assetTags.styles.map(tag => {
-                            if (/^enhanced.*\.css$/i.test(tag.attributes?.href)) {
-                                tag.attributes.rel = 'preload';
-                                tag.attributes.as = 'style';
-                                tag.attributes.onload = "this.onload=null;this.rel='stylesheet'";
-                            }
-                            return tag;
-                        }
-                    );
+                        // remove a link to critical CSS
+                        data.assetTags.styles = data.assetTags.styles.filter(
+                            (tag) =>
+                                !tag.attributes?.href?.includes("critical"),
+                        );
+                        data.assetTags.styles = data.assetTags.styles.map(
+                            (tag) => {
+                                if (
+                                    /^enhanced.*\.css$/i.test(
+                                        tag.attributes?.href,
+                                    )
+                                ) {
+                                    tag.attributes.rel = "preload";
+                                    tag.attributes.as = "style";
+                                    tag.attributes.onload =
+                                        "this.onload=null;this.rel='stylesheet'";
+                                }
+                                return tag;
+                            },
+                        );
 
-                    // add inline CSS inside <head>
-                    data.assetTags.styles.unshift(inlineStyleTag);
-                    return data;
-                }
-            );
-        });
+                        // add inline CSS inside <head>
+                        data.assetTags.styles.unshift(inlineStyleTag);
+                        return data;
+                    },
+                );
+            },
+        );
     }
 }
 
 module.exports = {
     mode: isProd ? "production" : "development",
-    devtool: isProd ? false : 'source-map', // Setting up source maps for faster debugging
+    devtool: isProd ? false : "source-map", // Setting up source maps for faster debugging
     entry: {
-        main: './src/main.js'
+        main: "./src/main.js",
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: isProd ? 'js/[name].[contenthash].js' : 'js/[name].js',
-        sourceMapFilename: 'maps/[file].map',
-        clean: true
+        path: path.resolve(__dirname, "dist"),
+        filename: isProd ? "js/[name].[contenthash].js" : "js/[name].js",
+        sourceMapFilename: "maps/[file].map",
+        clean: true,
     },
     resolve: {
         alias: {
-            '@i': path.resolve(__dirname, 'i/'),
-            '@fonts': path.resolve(__dirname, 'src/assets/fonts/'),
-            '@img': path.resolve(__dirname, 'src/assets/bootstrap/img/'),
+            "@i": path.resolve(__dirname, "i/"),
+            "@fonts": path.resolve(__dirname, "src/assets/fonts/"),
+            "@img": path.resolve(__dirname, "src/assets/bootstrap/img/"),
         },
-        extensions: ['.js'], // Specify only js file extension
+        extensions: [".js"], // Specify only js file extension
     },
 
     module: {
@@ -90,88 +101,118 @@ module.exports = {
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
-                    isDevServer ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    isDevServer ? "style-loader" : MiniCssExtractPlugin.loader,
                     {
-                        loader: 'css-loader',
-                        options: {sourceMap: true}
+                        loader: "css-loader",
+                        options: { sourceMap: true },
                     },
-                    ...(isProd ? [{
-                        loader: 'postcss-loader', // connect PostCSS
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    require('postcss-import'),
-                                    require('autoprefixer')(
-                                        {
-                                            overrideBrowserslist: ['last 2 versions', '> 1%'], // Настройка браузеров
-                                        }
-                                    ),
-                                    require('postcss-preset-env'),
-                                ],
-                            },
-                        },
-                    }] : []),
+                    ...(isProd
+                        ? [
+                              {
+                                  loader: "postcss-loader", // connect PostCSS
+                                  options: {
+                                      postcssOptions: {
+                                          plugins: [
+                                              require("postcss-import"),
+                                              require("autoprefixer")({
+                                                  overrideBrowserslist: [
+                                                      "last 2 versions",
+                                                      "> 1%",
+                                                  ], // Настройка браузеров
+                                              }),
+                                              require("postcss-preset-env")({
+                                                  stage: 3,
+                                                  features: {
+                                                      'nesting-rules': false,
+                                                      'custom-properties': false,
+                                                      'logical-properties-and-values': false,
+                                                      'layer-pseudo-class': false, // важно!
+                                                  }
+                                              }),
+                                          ],
+                                      },
+                                  },
+                              },
+                          ]
+                        : []),
                     {
-                        loader: 'sass-loader',
-                        options: {sourceMap: true}
-                    }
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                            sassOptions: {
+                                includePaths: [
+                                    path.resolve(__dirname, 'src/styles'),
+                                    path.resolve(__dirname, 'node_modules')
+                                ]
+                            },
+                            additionalData: `
+                                @use "sass:map";
+                                @use "sass:math";
+                            `
+                        }
+                    },
                 ],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
                 generator: {
-                    filename: isProd ? 'assets/fonts/[name][ext][query]' : 'assets/fonts/[name][ext]',
+                    filename: isProd
+                        ? "assets/fonts/[name][ext][query]"
+                        : "assets/fonts/[name][ext]",
                 },
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
                 generator: {
-                    filename: isProd ? 'i/[name][hash][ext]' : 'i/[name][ext]',
+                    filename: isProd ? "i/[name][hash][ext]" : "i/[name][ext]",
                 },
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',     // for ES6+ support
+                    loader: "babel-loader", // for ES6+ support
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ["@babel/preset-env"],
                     },
                 },
             },
-
         ],
     },
     cache: {
-        type: 'filesystem',
+        type: "filesystem",
         buildDependencies: {
             config: [__filename],
         },
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html',
-            favicon: './i/favicon.ico',
+            template: "./index.html",
+            filename: "index.html",
+            favicon: "./i/favicon.ico",
             inject: true,
-            chunks: ['main'],
+            chunks: ["main"],
         }),
         ...(isProd ? [new optimizedCriticalandEnhancedCSSPlugin()] : []),
-        ...(!isDevServer ? [
-            new MiniCssExtractPlugin({
-                filename: (pathData) => getCssFilename(isProd, pathData.chunk.name),
-
-            })] : []),
+        ...(!isDevServer
+            ? [
+                  new MiniCssExtractPlugin({
+                      filename: (pathData) =>
+                          getCssFilename(isProd, pathData.chunk.name),
+                  }),
+              ]
+            : []),
         new ImageMinimizerPlugin({
+            //exclude: /(?:^|[/\\])bg-body[\w-]*\.png$/i,// Exclude specific images from optimization like this bg-bodyd0fbbb9d0ae0aef8bb65.png
             minimizer: {
                 implementation: ImageMinimizerPlugin.imageminGenerate, // Specify the optimization method
                 options: {
                     // Now specify optimizers via "presets"
                     plugins: [
-                        ["imagemin-mozjpeg", {quality: 75}], // JPEG compression
-                        ["imagemin-pngquant", {quality: [0.65, 0.9]}], // PNG
+                        ["imagemin-mozjpeg", { quality: 75 }], // JPEG compression
+                        ["imagemin-pngquant", { quality: 99-100 }], // PNG
                         [
                             "imagemin-svgo",
                             {
@@ -194,20 +235,20 @@ module.exports = {
         new CopyWebpackPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, 'i'), // Folder with images in the source
-                    to: 'i', // Target folder in dist
+                    from: path.resolve(__dirname, "i"), // Folder with images in the source
+                    to: "i", // Target folder in dist
                 },
-                {
-                    from: path.resolve(__dirname, 'src/assets/js'), // Folder with scripts in the source
-                    to: 'assets/js', // Target folder in dist
-                },
+                // {
+                //     from: path.resolve(__dirname, 'src/assets/js'), // Folder with scripts in the source
+                //     to: 'assets/js', // Target folder in dist
+                // },
             ],
         }),
     ],
     optimization: {
-        moduleIds: 'deterministic',
-        chunkIds: 'deterministic',
-        runtimeChunk: 'single',
+        moduleIds: "deterministic",
+        chunkIds: "deterministic",
+        runtimeChunk: "single",
         minimizer: [
             `...`, // Extend Webpack's default JS minimizes
             new TerserPlugin({
@@ -219,22 +260,21 @@ module.exports = {
                 extractComments: false, // Do not output separate files with comments
             }),
             new CssMinimizerPlugin(), // Minimize CSS
-
         ], // CSS minimization is enabled by default in Webpack 5
         splitChunks: {
             cacheGroups: {
                 critical: {
-                    name: 'critical', // For separating critical.scss
+                    name: "critical", // For separating critical.scss
                     test: /critical\.scss$/,
-                    chunks: 'all',
-                    enforce: false, // Force creating a separate bundle
+                    chunks: "all",
+                    enforce: true, // Force creating a separate bundle
                     priority: 2,
                 },
                 enhanced: {
-                    name: 'enhanced', // For separating enhanced.scss
+                    name: "enhanced", // For separating enhanced.scss
                     test: /enhanced\.scss$/,
-                    chunks: 'all',
-                    enforce: false, // Force creating a separate bundle
+                    chunks: "all",
+                    enforce: true, // Force creating a separate bundle
                     priority: 1,
                 },
                 default: false,
@@ -246,7 +286,7 @@ module.exports = {
     },
     devServer: {
         static: {
-            directory: path.resolve(__dirname, 'dist'),
+            directory: path.resolve(__dirname, "dist"),
             watch: true,
         },
         compress: true,
@@ -254,18 +294,20 @@ module.exports = {
         hot: true,
         liveReload: true,
         watchFiles: {
-            paths: ['index.html'],
+            paths: ["index.html"],
         },
         headers: {
-            'Cache-Control': 'no-store', // Disable caching
+            "Cache-Control": "no-store", // Disable caching
         },
         client: {
             overlay: false, // disable overlay with errors and warnings in the browser
         },
     },
-    performance: isProd ? {
-        hints: 'warning',
-        maxEntrypointSize: 512000,    // 500KB
-        maxAssetSize: 512000          // 500KB
-    } : false
+    performance: isProd
+        ? {
+              hints: "warning",
+              maxEntrypointSize: 512000, // 500KB
+              maxAssetSize: 512000, // 500KB
+          }
+        : false,
 };
