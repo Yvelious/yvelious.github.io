e5c6701e61a9cab4b25a1205593263a5255190b6
Stanislav Levyshchev <yvelious@gmail.com>
2025-09-10 15:51:00 +0200

    added WebP support in image handling configuration

--- webpack.config.js
+++ webpack.config.js
@@ -164,7 +164,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/i,
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
                 type: "asset/resource",
                 generator: {
                     filename: isProd ? "i/[name][hash][ext]" : "i/[name][ext]",
